b475611fbd1e3d84da81e5aef048f6fe785cf969
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2025-04-08 19:08:51 +0000

    include files for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production', // Change this to 'production' for optimized builds
   entry: './src/index.js',
   devServer: {
     static: './dist',
@@ -13,7 +13,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].js',
+    filename: '[name].[contenthash].js', // Use contenthash for cache busting
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
